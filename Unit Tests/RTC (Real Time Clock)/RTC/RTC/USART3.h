fc62248462e4ae832bf52cd30c0df668bdeb7f9a
Shrey Dahiya <113534668+CoolDude447@users.noreply.github.com>
2024-11-22 20:17:51 -0400

    Update USART3.h

--- Unit Tests/RTC (Real Time Clock)/RTC/RTC/USART3.h
+++ Unit Tests/RTC (Real Time Clock)/RTC/RTC/USART3.h
@@ -1,23 +1,3 @@
-/*
- * File:   USART3.h
- * Author: Armstrong Subero
- * Processor: SAMD21G18A @ 48MHz, 3.3v
- * Program: Header file for application
- * Compiler: ARM-GCC (v6.3.1, Atmel Studio 7.0)
- * Program Version 1.0
- * Program Description: This header file provides functions for USART
-                        communication
- * Modified From: None
- * 
- * Change History:
- *
- * Author             Rev     Date          Description
- * Armstrong Subero   1.0     01/06/2020    Initial Release.
- * 
- * Updated on June 01, 2020, 05:58 PM
- */
-
-
 #ifndef USART3_H_
 #define USART3_H_
 
