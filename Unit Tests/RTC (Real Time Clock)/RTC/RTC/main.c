964e162995d63eb5eb06c3bb2f72aa14fc17e2bf
Shrey Dahiya <113534668+CoolDude447@users.noreply.github.com>
2024-11-22 20:19:29 -0400

    Update main.c

--- Unit Tests/RTC (Real Time Clock)/RTC/RTC/main.c
+++ Unit Tests/RTC (Real Time Clock)/RTC/RTC/main.c
@@ -1,29 +1,3 @@
-/*
- * File:   main.c
- * Author: Armstrong Subero
- * Processor: SAMD21G18A @ 48MHz, 3.3v
- * Program: Source file for application
- * Compiler: ARM-GCC (v6.3.1, Atmel Studio 7.0)
- * Program Version 1.0
- * Program Description: This is the main file
- * Hardware Description: A CP2014 USB-UART module is connected through a logic
- *                       level converter as follows:
- *
- *                       GND -> GND
- *                       TX -> PIN32 (PA23)   
- *                       RX -> PIN31 (PA22)
- *
- * Modified From: None
- * 
- * Change History:
- *
- * Author                Rev     Date          Description
- * Armstrong Subero      1.1     26/05/2020    Initial Release
- *
- * Updated on May 26, 2020, 11:51 AM
- */
-
-
 //////////////////////////////////////////////////////////////////////////
 // Include and defines
 //////////////////////////////////////////////////////////////////////////
